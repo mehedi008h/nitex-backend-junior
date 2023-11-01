cc8a77bbc6e0f2654afb20a1fa76dbdad447b005
Mehedi Hasan <mehedi08h@gmail.com>
2023-11-01 22:55:18 +0600

    project setup

---
+++ src/main/java/com/mehedi/nitex/NitexApplication.java
@@ -0,0 +1,13 @@
+package com.mehedi.nitex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitexApplication.class, args);
+    }
+
+}
